7e1f1ff0df05369b819ce337d590ca9618c98059
CoderMayhem <namanmishra1900@gmail.com>
2023-05-01 22:42:38 +0530

    titan init

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3030;
 app.use('/mayhem', express.static(__dirname + '/mayhem'));
+app.use('/titan', express.static(__dirname + '/titan'));
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
