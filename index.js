5fc4460510ddb39eef2a09044c9363a46a8f591c
CoderMayhem <namanmishra1900@gmail.com>
2023-05-02 13:03:38 +0530

    aa files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 app.use('/mayhem', express.static(__dirname + '/mayhem'));
 app.use('/titan', express.static(__dirname + '/titan'));
+app.use('/ayushagarwal', express.static(__dirname + '/ayushagarwal'));
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
