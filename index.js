bff5530bc53c5ef52775ccf036cf66a79b990345
CoderMayhem <namanmishra1900@gmail.com>
2023-05-14 18:17:36 +0530

    dheeraj edit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3030;
 app.use('/mayhem', express.static(__dirname + '/mayhem'));
 app.use('/titan', express.static(__dirname + '/titan'));
 app.use('/ayushagarwal', express.static(__dirname + '/ayushagarwal'));
+app.use('/dheerajbathla', express.static(__dirname + '/dheerajbathla'));
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
