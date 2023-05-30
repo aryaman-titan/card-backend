3f9750bd02e44458cbd4fe338b169bf687d14528
Aryaman Behera <aryamanbehera@gmail.com>
2023-05-30 14:27:51 +0530

    refactor code

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const fs = require('fs');
+
 const app = express();
-const PORT = process.env.PORT || 3030;
-app.use('/mayhem', express.static(__dirname + '/mayhem'));
-app.use('/titan', express.static(__dirname + '/titan'));
-app.use('/ayushagarwal', express.static(__dirname + '/ayushagarwal'));
-app.use('/dheerajbathla', express.static(__dirname + '/dheerajbathla'));
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+
+// Define the static files directory
+const staticFilesDirectory = path.join(__dirname, 'static');
+
+// Create a storage engine for multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const { username } = req.params;
+    const userStaticDirectory = path.join(staticFilesDirectory, username);
+    cb(null, userStaticDirectory);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+// Create an instance of multer with the storage engine
+const upload = multer({ storage });
+
+// Define a route for serving the user-specific static files
+app.use('/:username', (req, res) => {
+  const { username } = req.params;
+  const userStaticDirectory = path.join(staticFilesDirectory, username);
+  if (fs.existsSync(userStaticDirectory)) {
+    express.static(userStaticDirectory)(req, res);
+  } else {
+    res.status(404).send('User not found');
+  }
+} );
+
+// Define a route for uploading the zip file
+app.post('/upload/:username', upload.single('file'), (req, res) => {
+  res.status(200).send('File uploaded successfully');
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message || 'Internal Server Error');
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
